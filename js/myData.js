b75442a925d9dcf55f9bd70054fe80430011eb26
Gael Bau <gaerbau@gmail.com>
2019-03-06 15:42:15 -0800

    added initial pages for progress, recipe history, and weight with simple nav

--- js/myData.js
+++ js/myData.js
@@ -2,6 +2,7 @@ window.onload = function () {
     // sample outputs
     document.querySelector('#dietMetric').innerHTML = 'Keto';
     document.querySelector('#weightMetric').innerHTML = '125 lbs';
+    document.querySelector('#sexMetric').innerHTML = 'male';
     document.querySelector('#heightMetric').innerHTML = '5 \' 8\'\'';
     document.querySelector('#exerciseMetric').innerHTML = 'Lazy Boy';
     document.querySelector('#forbiddenMetric').innerHTML = 'Broccoli';
