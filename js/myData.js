04f0a2bbad99842f34cb4206b0d99644b07e15a1
Gael Bau <gaerbau@gmail.com>
2019-03-06 15:33:08 -0800

    added basic components for my data

---
+++ js/myData.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    // sample outputs
+    document.querySelector('#dietMetric').innerHTML = 'Keto';
+    document.querySelector('#weightMetric').innerHTML = '125 lbs';
+    document.querySelector('#heightMetric').innerHTML = '5 \' 8\'\'';
+    document.querySelector('#exerciseMetric').innerHTML = 'Lazy Boy';
+    document.querySelector('#forbiddenMetric').innerHTML = 'Broccoli';
+    document.querySelector('#nutrientMetric').innerHTML = 'Less Carbohydrates';
+    document.querySelector('#spiceMetric').innerHTML = '1/5';
+}
