db65af58e16396fd2dfbfb7f934ba1449a314fa5
Greg Knight <gaknight@uci.edu>
2019-03-08 14:39:34 -0800

    Hard-coded search results now appear in console when recommendation button is clicked

--- js/recipeRecs.js
+++ js/recipeRecs.js
@@ -1,4 +1,14 @@
 window.onload = function () {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			<!--document.getElementById("demo").innerHTML = this.responseText;-->
+			console.log(this.responseText);
+		}
+	};
+	xhttp.open("GET", 'http://api.yummly.com/v1/api/recipes?_app_id=bb0eb5d5&_app_key=4723b75a803f80f41d8b0cf018534045&q=onion+soup', true);
+	xhttp.send();
+	
     $('#recipe-recs-list').on('click','li', function (event) {
         console.log(event);
         let query = '';
@@ -9,6 +19,4 @@ window.onload = function () {
         
         location.href = `./recipe.html${query}`;
     });
-
-    console.log('hello');
 }
